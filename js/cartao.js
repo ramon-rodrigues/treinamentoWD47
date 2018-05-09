13a4577ccfa1a30e852dc23074c1ef71e02d6d55
Ramon Costa <ramon.c@samsung.com>
2018-05-09 14:24:58 -0300

    Iniciando Projeto Treinamento

---
+++ js/cartao.js
@@ -0,0 +1,40 @@
+(function(){
+    const cartoes = document.querySelectorAll('.cartao')
+    for (let index = 0; index < cartoes.length; index++) {
+        const cartao = cartoes[index]    
+
+        cartao.addEventListener('focusin', function(){
+            cartao.classList.add('cartao-focado')
+        })
+        cartao.addEventListener('focusin', function(){
+            cartao.classList.remove('cartao-focado')
+        })
+
+        cartao.addEventListener('change', function mudaCor(){
+            const elementoSelecionado = event.target
+            const isRadioTipo = elementoSelecionado.classList.contains('opcoesDoCartao-radioTipo')
+            if(isRadioTipo){
+                cartao.style.backgroundColor = elementoSelecionado.value
+            }
+        })
+
+        cartao.addEventListener("keydown", function deixaClicarComEnter(event){
+            if(event.target.classList.contains("opcoesDoCartao-opcao") && (event.key === 'Enter' || event.key === ' ')){ 
+                event.target.click()
+            }
+        })
+
+        cartao.addEventListener("click", function (event){
+            const elementoSelecionado = event.target
+            if (elementoSelecionado.classList.contains('opcoesDoCartao-remove')) {
+                cartao.classList.add('cartao--some')
+                cartao.addEventListener("transitionend", function(){
+                    cartao.remove()            
+                })
+            }
+        })
+        
+        
+
+    }
+})()
---
+++ js/opcoesDoCartao/remove.js
@@ -0,0 +1,17 @@
+// (function(){
+
+// // const btnRemove = document.querySelector('.opcoesDoCartao-remove')
+// const btnsRemove = document.querySelectorAll('.opcoesDoCartao-remove')
+// for (let index = 0; index < btnsRemove.length; index++) {
+//     btnsRemove[index].addEventListener('click', function(){
+//         const cartao = btnsRemove[index].parentNode.parentNode
+//         cartao.classList.add('cartao--some')
+//         cartao.addEventListener("transitionend", function(){
+//             cartao.remove()            
+//         })
+//     })
+// }
+
+// })()
+
+
